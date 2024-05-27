2305cf069a6008eb5f85e99b69f876e9031e904f
LimjeongSik <ol37@naver.com>
2024-05-27 16:37:58 +0900

    feat:test3

--- src/app/reservation/schedule/page.tsx
+++ src/app/reservation/schedule/page.tsx
@@ -8,15 +8,17 @@ import { getDates } from "@/services/reservationAction";
 const ScheduleSelectionPage: NextPage = async () => {
     const dates = await getDates("2", "0000001");
 
+    if (!dates) return <div>데이터가 없습니다.</div>;
+
     return (
         <>
             <Header
                 title="서울 상상나라 극장 - 고릴라 대모험 1"
                 headerLeft={<HeaderBackButton />}
             />
-            {/* <BasicLayout>
+            <BasicLayout>
                 <ScheduleContainer dates={dates} />
-            </BasicLayout> */}
+            </BasicLayout>
         </>
     );
 };
